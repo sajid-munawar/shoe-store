c23f19e2809bf0f3515a49fecb6b45a5f30821ad
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-13 12:06:19 +0500

    Displayed Home and About

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React from 'react';
+import {Home,About} from "./Components"
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
-function App (){
-    return (<div>
-        Hello
-    </div>)
+function App() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 export default App;
